b1dbd7e168ed345ca63b92f4fd0f724e621c313b
Trishan <sanjanatrishan@gmail.com>
2025-11-04 02:20:36 +0530

    refactor: reorganize user-related models and enhance ExpenseResponse structure

--- api/models.py
+++ api/models.py
@@ -1,8 +1,24 @@
 from pydantic import BaseModel
 import datetime
 
-class ExpenseBase(BaseModel):
+class UserRegister(BaseModel):
+    username: str
+    password: str
+    confirm_password: str
+    
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserResponse(BaseModel):
     id: int
+    username: str
+
+    class Config:
+        from_attributes = True
+
+class ExpenseCreate(BaseModel):
     description: str
     amount: float
     date: datetime.date
@@ -13,3 +29,14 @@ class ExpenseUpdate(BaseModel):
     amount: float | None = None
     date: datetime.date | None = None
     category: str | None = None
+    
+class ExpenseResponse(BaseModel):
+    id: int
+    description: str
+    amount: float
+    date: datetime.date
+    category: str
+    user_id: int
+    
+    class Config:
+        from_attributes = True
