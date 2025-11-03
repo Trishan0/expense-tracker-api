4e4115570f84307da3b453944c8cfe1d064b042d
Trishan <sanjanatrishan@gmail.com>
2025-11-03 10:00:11 +0530

    fix: add ForeignKey and relationship for user in Expense model

--- api/db_models.py
+++ api/db_models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String,  Float, DateTime
+from sqlalchemy import Column, Integer, String,  Float, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -11,3 +12,17 @@ class Expense(Base):
     amount = Column(Float)
     date = Column(DateTime)
     category = Column(String)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    
+    user = relationship("User", back_populates="expenses")
+    
+    
+class User(Base):
+    __tablename__ = "user"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String(100))
+    
+    expenses = relationship("Expense", back_populates="user")
+    
