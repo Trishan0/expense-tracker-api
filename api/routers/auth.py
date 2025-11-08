31be2f966fceb0e591fc36f51c712e064d0c0565
Trishan <sanjanatrishan@gmail.com>
2025-11-08 09:00:34 +0530

    feat: add login endpoint for user authentication

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -4,7 +4,7 @@ from api.db_functions import get_db
 import api.db_models as db_models
 import api.models as models
 from api.security import hash_password, verify_password, create_access_token
-
+from fastapi.security import OAuth2PasswordRequestForm
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/register", response_model=models.UserResponse, status_code=status.HTTP_201_CREATED,)
@@ -26,3 +26,21 @@ def register_user(user: models.UserRegister, db: Session = Depends(get_db)):
     db.refresh(new_user)
     
     return new_user
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(db_models.User).filter(db_models.User.username == form_data.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
+    
+    pwd_verified = verify_password(form_data.password, user.password)
+    
+    if not pwd_verified:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
+    
+    access_token = create_access_token(data={"sub":str(user.id)})
+    
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
