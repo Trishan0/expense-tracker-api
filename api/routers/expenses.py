72f3ef05da357c099ad76912e11ad245cc978d17
Trishan <sanjanatrishan@gmail.com>
2025-11-08 09:16:08 +0530

    feat: enforce user authentication for expense management endpoints

--- api/routers/expenses.py
+++ api/routers/expenses.py
@@ -5,6 +5,7 @@ from api.db_functions import init_db, get_db
 import api.db_models as db_models
 import api.models as models
 import logging
+from api.security import get_current_user
 
 logger = logging.getLogger(__name__)
 
@@ -12,21 +13,21 @@ router = APIRouter(prefix="/expenses", tags=["Expenses"])
 
 
 @router.get("/")
-def get_all_expenses(db:Session = Depends(get_db)):
-    db_expenses = db.query(db_models.Expense).all()
+def get_all_expenses(db:Session = Depends(get_db), current_user:db_models.User = Depends(get_current_user)):
+    db_expenses = db.query(db_models.Expense).filter(db_models.Expense.user_id == current_user.id).all()
     return db_expenses
     
 
 @router.get("/{expense_id}")
-def get_expense_by_id(expense_id:int, db:Session = Depends(get_db)):
-    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id).first()
+def get_expense_by_id(expense_id:int, db:Session = Depends(get_db), current_user: db_models.User = Depends(get_current_user)):
+    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id, db_models.Expense.user_id == current_user.id).first()
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
     return expense
 
 @router.post("/")
-def create_expense(expense:models.ExpenseCreate, db:Session = Depends(get_db)):
-    db_expense = db_models.Expense(**expense.model_dump())
+def create_expense(expense:models.ExpenseCreate, db:Session = Depends(get_db), current_user:db_models.User = Depends(get_current_user)):
+    db_expense = db_models.Expense(**expense.model_dump(),user_id = current_user.id)
     db.add(db_expense)
     db.commit()
     db.refresh(db_expense)  
@@ -34,8 +35,8 @@ def create_expense(expense:models.ExpenseCreate, db:Session = Depends(get_db)):
     return db_expense
 
 @router.patch("/{expense_id}")
-def partial_update_expense(expense_id: int, updated_fields: models.ExpenseUpdate, db: Session = Depends(get_db)):
-    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id).first()
+def partial_update_expense(expense_id: int, updated_fields: models.ExpenseUpdate, db: Session = Depends(get_db), current_user:db_models.User= Depends(get_current_user)):
+    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id, db_models.Expense.user_id == current_user.id).first()
     
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
@@ -53,8 +54,8 @@ def partial_update_expense(expense_id: int, updated_fields: models.ExpenseUpdate
     
  
 @router.delete("/{expense_id}")
-def delete_expense(expense_id:int, db:Session = Depends(get_db)):
-    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id).first()
+def delete_expense(expense_id:int, db:Session = Depends(get_db), current_user:db_models.User = Depends(get_current_user)):
+    expense = db.query(db_models.Expense).filter(db_models.Expense.id == expense_id, db_models.Expense.user_id == current_user.id).first()
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
     db.delete(expense)
